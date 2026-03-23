7da1b89d3fd9d9bdfcedc93d1f928d7362ade2fb
Jinyoung Jung <jy@nai2.tail71e153.ts.net>
2026-03-23 16:19:08 +0900

    auto: 1c944690 edited __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .modules import Attention, PreNorm, FeedForward, GEGLU, Decoder
 from .sparceiver import SubjectAwareSparceiver
 from .sparceiver_multilabel import MultiLabelSparceiver
+from .sparceiver_io import SparceiverIO
